90893e85632675aa0d3afd8fd385bfa332380d86
Muhammad Irwanto <muhammad.irwanto95@gmail.com>
2020-08-02 14:45:32 +0700

    Set externals

--- make/make_externals.bat
+++ make/make_externals.bat
@@ -13,30 +13,30 @@ echo .
 set CONTAINER=mufise
 set TAG=%CONTAINER%.tag
 set URL=https://github.com/irwanto95/mufise.git/trunk/mufise
-
+pause
 if  not exist %DEST_FOLDER%\%TAG% (
 	break > %DEST_FOLDER%\%TAG%
 
 	svn propset svn:externals "%CONTAINER% %URL%" %DEST_FOLDER%
+	svn ci -m "Set externals"
 )
-
-pause
 
 REM echo ===========================================================
 echo Making externals .. libxls
 echo .
 set CONTAINER=libxls
 set TAG=%CONTAINER%.tag
-set URL=https://github.com/libxls/libxls/releases/tag/v1.5.3
+set URL=https://github.com/libxls/libxls.git//releases/tag/v1.5.3
+pause
 
 if  not exist %DEST_FOLDER%\%TAG% (
 	break > %DEST_FOLDER%\%TAG%
 
 	svn propset svn:externals "%CONTAINER% %URL%" %DEST_FOLDER%
+	svn ci -m "Set externals"
 )
 
-pause
-
+REM echo ===========================================================
 echo Update folder .. %DEST_FOLDER%
 svn update %DEST_FOLDER%
 
